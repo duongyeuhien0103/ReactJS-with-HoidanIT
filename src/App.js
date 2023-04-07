ee946db59c512aa35fe7d4e98b7e6c8dec57657e
Dương Nguyễn <Anhvip001hn@gmail.com>
2023-04-07 08:49:01 +0700

    prod day 1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-import logo from './logo.svg';
+
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-// import DisplayInfor from './components/DisplayInfor';
+
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return (
-      // <React.StrictMode>
-      <div>
 
 
-        <MyComponent />
-      </div>
-      // {/* </React.StrictMode> */}
-    );
-  }
+
+const App = () => {
+
+  return (
+    <div className='app-container'>
+      Hello world
+
+    </div>
+
+  );
 }
-
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       Hello world
-//       <MyComponent />
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       BEGINER ReactJS
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// }
 export default App;
